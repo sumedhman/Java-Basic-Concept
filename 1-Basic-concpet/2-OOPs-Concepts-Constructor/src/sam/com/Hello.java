b1d7f81736d257ff55e103a4d5bd3f3b8543e36d
sumedh <37269510+sumedhman@users.noreply.github.com>
2022-09-21 19:17:45 +0530

    Add files via upload

---
+++ 1-Basic-concpet/2-OOPs-Concepts-Constructor/src/sam/com/Hello.java
@@ -0,0 +1,12 @@
+package sam.com;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car c =new Car("closed","on","seated",10);
+		//Car c =new Car();
+		System.out.println(c.run());
+	}
+
+}
